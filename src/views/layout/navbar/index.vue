13e51a38446849a29faee0236cf4fc3020a0259b
83742368@qq.com <83742368@qq.com>
2020-06-10 13:47:38 +0800

    注释代码

--- src/views/layout/navbar/index.vue
+++ src/views/layout/navbar/index.vue
@@ -16,14 +16,14 @@
             <use xlink:href="#icon-outdent" />
           </svg>
         </el-menu-item>
-        <el-menu-item index="2" @click="refresh()">
+        <!-- <el-menu-item index="2" @click="refresh()">
           <svg
             class="icon-svg aui-navbar__icon-menu aui-navbar__icon-menu--refresh"
             aria-hidden="true"
           >
             <use xlink:href="#icon-sync" />
           </svg>
-        </el-menu-item>
+        </el-menu-item>-->
       </el-menu>
       <el-menu class="aui-navbar__menu" mode="horizontal">
         <el-menu-item index="1">
